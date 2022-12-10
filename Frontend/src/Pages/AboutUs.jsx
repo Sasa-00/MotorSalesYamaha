bd5cae5b900e9f10043d4e39e0f2cc09144a5a40
sasa <miljatovicsasa9@gmail.com>
2022-12-10 17:16:37 +0100

    FInished About and Contact pages

--- Frontend/src/Pages/AboutUs.jsx
+++ Frontend/src/Pages/AboutUs.jsx
@@ -10,7 +10,26 @@ import about7 from './../Assets/about/about7.jpg'
 import about8 from './../Assets/about/about8.jpg'
 import about9 from './../Assets/about/about9.jpg'
 
+import { useInView } from 'react-intersection-observer'
+
 const AboutUs = () => {
+
+  const oprions = {
+    threshold: 0.9,
+    triggerOnce: true
+  }
+
+  const { ref: myRef1, inView: divIsVisible1 } = useInView(oprions);
+  const { ref: myRef2, inView: divIsVisible2 } = useInView(oprions);
+  const { ref: myRef3, inView: divIsVisible3 } = useInView(oprions);
+  const { ref: myRef4, inView: divIsVisible4 } = useInView(oprions);
+  const { ref: myRef5, inView: divIsVisible5 } = useInView(oprions);
+  const { ref: myRef6, inView: divIsVisible6 } = useInView(oprions);
+  const { ref: myRef7, inView: divIsVisible7 } = useInView(oprions);
+  const { ref: myRef8, inView: divIsVisible8 } = useInView(oprions);
+  const { ref: myRef9, inView: divIsVisible9 } = useInView(oprions);
+
+
   return (
     <div className='about-gradient'>
       <div className='about-container'>
@@ -18,7 +37,7 @@ const AboutUs = () => {
         <br />
         <hr />
         <br /><br />
-        <div className='about-card'>
+        <div ref={myRef1} className={divIsVisible1? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about1} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1955</h1>
@@ -26,7 +45,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Prvi Yamahin motorizirani proizvod bio je motocikl YA-1 (125cc, 2-taktni, jednocilindrični, ulični bicikl). Pod nadimkom "Crveni vilin konjic", proizveden je i prodavan u Japanu. Potpuno novi YA-1 pobijedio je u prve dvije trke u kojima je učestvovao te godine.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef2} className={divIsVisible2? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about2} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1958</h1>
@@ -34,7 +53,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Prve Yamaha motocikle prodate u SAD-u je uvezao Cooper Motors, nezavisni distributer. Modeli su bili YD1 (250cc, 2-taktni, dvocilindrični, streetbike) i MF-1 (50cc, 2-taktni, jednocilindrični, streetbike, step-through). Yamaha ulazi u svijet međunarodnih utrka takmičeći se na Grand Prixu Catalina u Kaliforniji. U ovom prvom pokušaju protiv iskusnih svjetskih trkaćih mašina, Yamaha je zauzela impresivno 6. mjesto. Ova utrka je bila prva utrka japanskog motocikla u SAD-u.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef3} className={divIsVisible3? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about3} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1960</h1>
@@ -42,7 +61,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Yamaha predstavlja svoj prvi vanbrodski motor, P-7, za japansko tržište. Ubrzo je zamijenjen sofisticiranijim i kompaktnijim modelom P-3. Oba su imala prepoznatljive, jarko žute navlake. Yamaha International Corporation počela je prodavati motocikle u SAD-u, kao i Yamahine legendarne muzičke instrumente.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef4} className={divIsVisible4? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about4} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1961</h1>
@@ -50,7 +69,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Yamahino prvo pojavljivanje u svjetskim GP cestovnim utrkama u 3. kolu VN Francuske. Zauzeo je 6. mjesto u klasi World GP 250cc u 4. kolu TT utrke Isle of Man u Britaniji.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef5} className={divIsVisible5? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about5} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1963</h1>
@@ -58,7 +77,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Yamaha sarađuje sa Toyotom na razvoju i proizvodnji Toyote 2000GT sportskog automobila. Modifikovani kabriolet 2000GT prikazan je u filmu o Džejmsu Bondu iz 1967. godine “You Only Live Twice”. Yamaha osvaja svoju prvu pobjedu u klasi 250cc na Grand Prixu u Daytoni.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef6} className={divIsVisible6? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about6} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1968</h1>
@@ -66,7 +85,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Predstavljen je prvi Yamahin motor za sanke, SL350 (2-taktni, dvocilindrični). Ovo je bila prva motorna sanka sa karburatorima sa kliznim ventilom. DT-1 Enduro je predstavljen. Prvi motocikl dvostruke namjene na svijetu imao je sposobnost vožnje i van nje. Njegov uticaj na motociklizam u SAD bio je ogroman.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef7} className={divIsVisible7? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about7} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1970</h1>
@@ -74,7 +93,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Predstavljen je prvi Yamahin 4-taktni model motocikla, XS-1 (650cc vertikalni twin). Proizvodnja modela XS650 nastavila bi se kroz 1983. modelsku godinu, a motocikl je danas favoriziran kod kupaca.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef8} className={divIsVisible8? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about8} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>1980</h1>
@@ -82,7 +101,7 @@ const AboutUs = () => {
             <p className='about-card-p'>Nova kancelarija Yamaha Motor Corporation, SAD, otvorena je u Cypressu u Kaliforniji. Prvi Yamaha ATV sa 3 točka se prodaje u SAD... Tri-Moto (YT125). Predstavljen je G1-E model Golf automobila na električni pogon.</p>
           </div>
         </div>
-        <div className='about-card'>
+        <div ref={myRef9} className={divIsVisible9? 'appear about-card': 'about-card'}>
           <img className='about-card-img' src={about9} alt="motorcycle" />
           <div className='about-card-div'>
             <h1 className='about-card-year'>2008</h1>
--- Frontend/src/Pages/Contact.jsx
+++ Frontend/src/Pages/Contact.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import './../Styles/Contact.css'
+import { useState } from 'react'
+
 
 const Contact = () => {
+
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${email}`)
+    setEmail("")
+  }
+
   return (
     <div className='contact-container'>
       <div className='contact-input'>
-        <h1>inputs</h1>
+        <h3 className='contact-title'>OVDE NAM PIŠITE</h3>
+        <form className='contact-form' onSubmit={handleSubmit}>
+          <textarea className='textArea' rows="8" cols="50" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button className='contact-btn' type="submit">Pošalji</button>
+        </form>
+        <br /><br /><hr /><br /><br />
+        <i class="fa fa-mobile fa-lg" aria-hidden="true"> Telefon: <span className='bold'>064 12XXXXX</span></i>
+        <br /><br />
+        <i class="fa fa-envelope fa-lg" aria-hidden="true"> Email: <span className='bold'>yamahasrbija@gmail.com</span></i>
+        <br /><br />
+        <i class="fa fa-map-marker fa-lg" aria-hidden="true"> Adresa: <span className='bold'>Japanska 3a 11070 Novi Beograd</span></i>
       </div>
       <div className='contact-map'>
-        
+      <iframe className='map' frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=yamaha barel beograd&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
     </div>
   )
